aa5bac579dd4dfb1acede6331ee81a351b71fd13
fhajims <jakob.arneitz@edu.fh-joanneum.at>
2024-12-17 11:53:28 +0100

    Added image controller functionality

---
+++ src/main/java/com/birds/Birds/controller/ImageController.java
@@ -0,0 +1,64 @@
+package com.birds.Birds.controller;
+
+
+import com.birds.Birds.service.ServiceInterfaces.IImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final IImageService imageService;
+
+
+    String baseDir = System.getProperty("user.dir");
+    String subDir = "/src/main/java/com/birds/Birds/Files";
+
+    @PostMapping("/upload")
+    public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        return imageService.saveImage(file);
+    }
+
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> loadImage(@PathVariable String filename) {
+        try {
+            Path path = Paths.get(baseDir + subDir).resolve(filename);
+            Resource resource = new UrlResource(path.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                MediaType mediaType = MediaType.IMAGE_JPEG; // Default to JPEG
+                String contentType = resource.getFile().getName().toLowerCase();
+                if (contentType.endsWith(".png")) {
+                    mediaType = MediaType.IMAGE_PNG;
+                } else if (contentType.endsWith(".gif")) {
+                    mediaType = MediaType.IMAGE_GIF;
+                }
+
+                return ResponseEntity.ok()
+                        .contentType(mediaType)
+                        .body(resource);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/main/java/com/birds/Birds/repository/ImageRepository.java
+++ src/main/java/com/birds/Birds/repository/ImageRepository.java
@@ -1,8 +1,9 @@
 package com.birds.Birds.repository;
 
+import com.birds.Birds.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/*
+
+
 public interface ImageRepository extends JpaRepository<Image, Long> {
 }
-*/
